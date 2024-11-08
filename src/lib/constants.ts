d7c2279ce9137c676299594e954caff5e26f46a9
Oktavian Bayu Widodo <oktavian.konsultan@work.bri.co.id>
2024-11-08 15:37:19 +0700

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const BRI_URL = [
   "https://bridevstudio.bbri.io",
   "https://bridevstudio.dev.bbri.io",
 ];
-export const BASE_URL = "https://example-isolated-app.vercel.app";
+export const BASE_URL = "https://hello-world-external-app.vercel.app";
 export const SUPERADMIN_CREDENTIALS = {
   name: "Admin",
   email: "superadmin@bbri.io",
